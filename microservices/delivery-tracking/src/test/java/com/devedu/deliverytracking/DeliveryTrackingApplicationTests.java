f1ad606879ad863c780c90b9faa6aa9cc7ff1c38
Eduguimara <eduguimara@gmail.com>
2026-01-30 20:59:35 -0300

    Initial commit: Courier and Delivery microservices

---
+++ microservices/delivery-tracking/src/test/java/com/devedu/deliverytracking/DeliveryTrackingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devedu.deliverytracking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeliveryTrackingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
