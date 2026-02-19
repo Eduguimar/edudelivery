15906080a669b87e7cd390fb03465daae238894f
Eduguimara <eduguimara@gmail.com>
2026-02-18 22:18:07 -0300

    Feature: Create Get endpoint and action endpoints

--- microservices/delivery-tracking/src/main/java/com/devedu/deliverytracking/api/controller/DeliveryController.java
+++ microservices/delivery-tracking/src/main/java/com/devedu/deliverytracking/api/controller/DeliveryController.java
@@ -1,11 +1,17 @@
 package com.devedu.deliverytracking.api.controller;
 
+import com.devedu.deliverytracking.api.model.CourierIdInput;
 import com.devedu.deliverytracking.api.model.DeliveryInput;
 import com.devedu.deliverytracking.domain.model.Delivery;
+import com.devedu.deliverytracking.domain.repository.DeliveryRepository;
 import com.devedu.deliverytracking.domain.service.DeliveryPreparationService;
 import jakarta.validation.Valid;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
+import org.springframework.data.web.PagedModel;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.UUID;
 
@@ -14,9 +20,11 @@ import java.util.UUID;
 public class DeliveryController {
 
     private final DeliveryPreparationService deliveryService;
+    private final DeliveryRepository deliveryRepository;
 
-    public DeliveryController(DeliveryPreparationService deliveryService) {
+    public DeliveryController(DeliveryPreparationService deliveryService, DeliveryRepository deliveryRepository) {
         this.deliveryService = deliveryService;
+        this.deliveryRepository = deliveryRepository;
     }
 
 
@@ -29,5 +37,30 @@ public class DeliveryController {
     @PutMapping("/{deliveryId}")
     public Delivery edit(@PathVariable UUID deliveryId, @RequestBody @Valid DeliveryInput input) {
         return deliveryService.edit(deliveryId, input);
+    }
+
+    @GetMapping
+    public PagedModel<Delivery> findAll(@PageableDefault Pageable pageable) {
+        return new PagedModel<>(deliveryRepository.findAll(pageable));
+    }
+
+    @GetMapping("/{deliveryId}")
+    public Delivery findById(@PathVariable UUID deliveryId) {
+        return deliveryRepository.findById(deliveryId).orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND));
+    }
+
+    @PostMapping("/{deliveryId}/placement")
+    public void place(@PathVariable UUID deliveryId) {
+
+    }
+
+    @PostMapping("/{deliveryId}/pickups")
+    public void pickup(@PathVariable UUID deliveryId, @Valid @RequestBody CourierIdInput input) {
+
+    }
+
+    @PostMapping("/{deliveryId}/completion")
+    public void complete(@PathVariable UUID deliveryId) {
+
     }
 }
---
+++ microservices/delivery-tracking/src/main/java/com/devedu/deliverytracking/api/model/CourierIdInput.java
@@ -0,0 +1,16 @@
+package com.devedu.deliverytracking.api.model;
+
+import java.util.UUID;
+
+public class CourierIdInput {
+
+    private UUID courierId;
+
+    public UUID getCourierId() {
+        return courierId;
+    }
+
+    public void setCourierId(UUID courierId) {
+        this.courierId = courierId;
+    }
+}
